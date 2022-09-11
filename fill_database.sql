b9761a7208554e9813e3df3b8528653ab4f79dd1
Alexander <gallade2014@gmail.com>
2022-09-11 00:05:19 -0600

    Added a file to store sql queries.

---
+++ fill_database.sql
@@ -0,0 +1,49 @@
+use salvadorean_flavor;
+
+INSERT INTO
+    `ingredients` (
+        `ingredient_id`,
+        `name`,
+        `image`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'Tomate',
+        'default.png',
+        CURRENT_DATE(),
+        CURRENT_DATE()
+    );
+
+INSERT INTO
+    `ingredients` (
+        `ingredient_id`,
+        `name`,
+        `image`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'Ajo',
+        'default.png',
+        CURRENT_DATE(),
+        CURRENT_DATE()
+    );
+
+INSERT INTO
+    `ingredients` (
+        `ingredient_id`,
+        `name`,
+        `image`,
+        `created_at`,
+        `updated_at`
+    )
+VALUES (
+        NULL,
+        'Cebolla',
+        'default.png',
+        CURRENT_DATE(),
+        CURRENT_DATE()
+    );
