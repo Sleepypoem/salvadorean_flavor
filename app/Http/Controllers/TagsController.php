a30bfc0fafc6ba5b8e9b449795893fe19a84ba31
Maira2789 <mayte270597@gmail.com>
2022-09-11 23:51:14 -0600

    aCommit de funcionalidades de los Tags

---
+++ app/Http/Controllers/TagsController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Tags;
+
+class TagsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags=Tags::all();
+        return $tags;
+    }
+
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tags= new Tags();
+        $tags->recipe_id =$request->recipe_id;
+        $tags->name = $request->name;
+        $tags->save();
+
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $tags= Tags::findOrFail($id);
+        $tags->recipe_id =$request->recipe_id;
+        $tags->name = $request->name;
+        $tags->save();
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Tags::destroy($request->id);
+    }
+}
