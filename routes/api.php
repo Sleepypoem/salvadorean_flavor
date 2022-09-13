d6374c64b49ac50ae56111b98833063a007a40ff
glendagarcia99 <jeanmilletteg99@gmail.com>
2022-09-12 20:06:52 -0600

    ingredient routes

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,9 @@ Route::get("/v1/recipes", [RecipeController::class, "index"]);
 
 /* *************************************************************** Ingredient routes ************************************************************** */
 Route::get("/v1/ingredients", [IngredientController::class, "index"]);
+Route::post("/v1/ingredient", [IngredientController::class, "store"]);
+Route::put("/v1/ingredient/{id}", [IngredientController::class, "update"]);
+Route::delete("/v1/ingredient/{id}", [IngredientController::class, "destroy"]);
 /* ************************************************************************************************************************************************ */
 /* *************************************************************** Tags routes ************************************************************** */
 Route::get("/v1/tags", [TagsController::class, "index"]);
