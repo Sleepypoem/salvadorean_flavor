5fa8472cf5e7588488f89ef78ff7653a13427124
Maira2789 <mayte270597@gmail.com>
2022-09-13 19:12:00 -0600

    Added Categories Model and Controller

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Categories;
+
+class CategoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = Categories:: all();
+        return $categories;
+    }
+
+    
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $categories= new Categories();
+        $categories->category_id = $request->category_id;
+        $categories->name = $request->name;
+        $categories->save();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $categories = Categories::findOrFail($id);
+        $categories->category_id = $request->category_id;
+        $categories->name = $request->name;
+        $categories->save();
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Categories::destroy($id);
+    }
+}
